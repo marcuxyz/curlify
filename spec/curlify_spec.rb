43df9ff6314198678f079bd763c69fad4801b613
Marcus Pereira <mpereirassa@gmail.com>
2023-09-01 14:15:54 -0300

    🔧 fix(curlify_spec.rb): fix indentation and formatting issues in curlify_spec.rb ✨ feat(curlify_spec.rb): refactor tests to use shared response variable for better readability and maintainability

--- spec/curlify_spec.rb
+++ spec/curlify_spec.rb
@@ -7,51 +7,55 @@ require './lib/curlify'
 describe Curlify do
   let(:context) { described_class.new(request) }
   let(:uri) { URI('http://127.0.0.1') }
+  let(:response) do
+    <<~CURL
+      curl -X #{method} \
+      -H 'content-type: application/json' \
+      -H 'accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3' \
+      -H 'accept: */*' -H 'user-agent: Ruby' \
+      -H 'host: 127.0.0.1' #{body} http://127.0.0.1
+    CURL
+  end
 
   describe 'must transform request into curl command' do
     context '#get' do
       let(:request) { Net::HTTP::Get.new(uri, { 'content-type': 'application/json' }) }
+      let(:method)  { 'GET' }
+      let(:body)    { nil }
 
-      it 'should return curl command to GET request' do
-        expect(context.to_curl).to eq "curl -X GET -H 'content-type: application/json' -H 'accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3' -H 'accept: */*' -H 'user-agent: Ruby' -H 'host: 127.0.0.1'  http://127.0.0.1"
-      end
+      it { expect(context.to_curl).to eq response.strip }
     end
 
     context '#post' do
-      let(:request) do
-        Net::HTTP::Post.new(uri, { 'content-type': 'application/json' })
-      end
+      let(:request) { Net::HTTP::Post.new(uri, { 'content-type': 'application/json' }) }
+      let(:method)  { 'POST' }
+      let(:body)    { "-d #{payload}" }
+      let(:payload) { { name: 'John' }.to_json }
 
-      before do
-        request.body = { name: 'John' }.to_json
-      end
+      before { request.body = payload }
 
-      it 'should return curl command to Post request' do
-        expect(context.to_curl).to eq "curl -X POST -H 'content-type: application/json' -H 'accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3' -H 'accept: */*' -H 'user-agent: Ruby' -H 'host: 127.0.0.1' -d {\"name\":\"John\"} http://127.0.0.1"
-      end
+      it { expect(context.to_curl).to eq response.strip }
     end
 
     context '#put' do
       let(:request) { Net::HTTP::Put.new(uri, { 'content-type': 'application/json' }) }
+      let(:method) { 'PUT' }
+      let(:body)   { "-d #{payload}" }
+      let(:payload)         { { name: 'John', userId: 1 }.to_json }
 
-      before do
-        request.body = { name: 'John', userId: 1 }.to_json
-      end
+      before { request.body = payload }
 
-      it 'should return curl command to Put request' do
-        expect(context.to_curl).to eq "curl -X PUT -H 'content-type: application/json' -H 'accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3' -H 'accept: */*' -H 'user-agent: Ruby' -H 'host: 127.0.0.1' -d {\"name\":\"John\",\"userId\":1} http://127.0.0.1"
-      end
+      it { expect(context.to_curl).to eq response.strip }
     end
     context '#delete' do
       let(:request) { Net::HTTP::Delete.new(uri, { 'content-type': 'application/json' }) }
+      let(:method) { 'DELETE' }
+      let(:body)   { "-d #{payload}" }
+      let(:payload)         { { userId: 1 }.to_json }
 
-      before do
-        request.body = { userId: 1 }.to_json
-      end
+      before { request.body = payload }
 
-      it 'should return curl command to Delete request' do
-        expect(context.to_curl).to eq "curl -X DELETE -H 'content-type: application/json' -H 'accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3' -H 'accept: */*' -H 'user-agent: Ruby' -H 'host: 127.0.0.1' -d {\"userId\":1} http://127.0.0.1"
-      end
+      it { expect(context.to_curl).to eq response.strip }
     end
   end
 end
