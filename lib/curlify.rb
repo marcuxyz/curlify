d33eeac091b7711f37b3e700d44dc8f4feccd7bb
Marcus Almeida <mpereirassa@gmail.com>
2025-11-20 22:19:02 -0300

    feat: add clipboard support to Curlify for copying curl commands

--- lib/curlify.rb
+++ lib/curlify.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
 require 'faraday'
+require 'byebug'
 
 class Curlify
-  attr_reader :request, :verify, :compressed
+  attr_reader :request, :verify, :compressed, :clipboard
 
-  def initialize(request, compressed: false, verify: true)
+  def initialize(request, compressed: false, verify: true, clipboard: false)
     @request = request
     @compressed = compressed
     @verify = verify
+    @clipboard = clipboard
   end
 
   def to_curl
-    return "#{curl_request} --compressed" if compressed
-    return "#{curl_request} --insecure" unless verify
+    string = curl_request
+    string += '--insecure ' unless verify
+    string += '--compressed' if compressed
 
-    curl_request
+    copy_to_clipboard(string)
+    string
   end
 
   private
 
   def curl_request
-    "curl -X #{http_method.upcase} #{headers} #{body} #{url}"
+    "curl -X #{http_method.upcase} #{headers} #{body} #{url} "
   end
 
   def headers
@@ -46,5 +50,25 @@ class Curlify
 
   def context_headers(headers)
     headers.map { |k, v| "-H '#{k}: #{v}'" }.join(' ')
+  end
+
+  def copy_to_clipboard(string)
+    return unless clipboard
+
+    command = clipboard_command
+    return warn("Curlify Warning: 'xclip' is required for clipboard support on Linux.") unless command
+
+    IO.popen(command, 'w') { |f| f << string }
+  end
+
+  def clipboard_command
+    case RUBY_PLATFORM
+    when /darwin/
+      'pbcopy'
+    when /mswin|mingw|cygwin/
+      'clip'
+    when /linux/
+      'xclip -selection clipboard' if system('which xclip > /dev/null 2>&1')
+    end
   end
 end
