390488951409523076462956961f58361f3b18b5
Marcus Almeida <mpereirassa@gmail.com>
2025-11-22 12:34:01 -0300

    feat: refactor Settings class to streamline YAML configuration loading and add specs

--- lib/settings.rb
+++ lib/settings.rb
@@ -4,17 +4,14 @@ class Settings
   ROOT_DIR = File.expand_path('..', __dir__)
 
   def self.call
-    new.send(:load_yaml_config_file)
+    new.send(:params)
   end
 
   private
 
-  def load_yaml_config_file
-    YAML.load(read_file).transform_keys(&:to_sym)
-  end
+  def params
+    read_file = File.join(ROOT_DIR, 'config', 'settings.yaml')
 
-  def read_file
-    config_path = File.join(ROOT_DIR, 'config', 'settings.yaml')
-    File.read(config_path)
+    @params ||= YAML.load_file(read_file)
   end
 end
--- spec/lib/settings_spec.rb
+++ spec/lib/settings_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe '.call' do
   context 'when settings file exists' do
     let(:expected_settings) do
       {
-        compressed: false,
-        verify: true,
-        clipboard: false
+        'compressed' => false,
+        'verify' => true,
+        'clipboard' => false
       }
     end
 
@@ -17,7 +17,7 @@ RSpec.describe '.call' do
 
   context 'when settings file does not exist' do
     before do
-      allow_any_instance_of(Settings).to receive(:read_file).and_raise(Errno::ENOENT)
+      allow_any_instance_of(Settings).to receive(:params).and_raise(Errno::ENOENT)
     end
 
     it 'raises Errno::ENOENT' do
